8f4246ef7c2aea15a2a94ce8bfd28580536dd1c3
A014-014\user <2024_n-masuya_ncast@techseed.jp>
2024-06-05 08:51:27 +0000

    error

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,42 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
+import { Client } from "https://deno.land/x/mysql/mod.ts";
+
+interface User {
+  id: number;
+  password: string;
+  name: string;
+  admin: string;
+}
+
+const connectionParam = {
+  hostname: "db",
+  username: "ユーザー名",
+  password: "パスワード",
+  db: "testdb",
+
+};
+
+const client = await new Client().connect({
+  hostname: "172.17.0.1",
+  username: "seed",
+  db: "techdb",
+  password: "Tech_123",
+  port:33062
+});
+
+await client.execute(`
+  CREATE TABLE IF NOT EXISTS users (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      name varchar(100) NOT NULL,
+      created_at timestamp not null default current_timestamp,
+      PRIMARY KEY (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`);
 
 export default function Home() {
   const count = useSignal(3);
+
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -18,6 +52,7 @@ export default function Home() {
           FRESH
           <code class="mx-2">BOY</code> NANAKI
         </p>
+        <p>{user}</p>
         <Counter count={count} />
       </div>
     </div>
